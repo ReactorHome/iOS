1e8d508fdb32c16208a64475ee7eb1c8e0b52b41
Will <will@mockdesign.com>
2018-01-22 16:30:39 -0600

    Updating storyboard

---
+++ ReactorHome/ReactorHome/AlertTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlertTableViewCell.swift
+//  ReactorHome
+//
+//  Created by Will Mock on 10/17/17.
+//  Copyright © 2017 Mock. All rights reserved.
+//
+
+import UIKit
+
+class AlertTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ReactorHome/ReactorHome/DeviceControlTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DeviceControlTableViewCell.swift
+//  ReactorHome
+//
+//  Created by Will Mock on 10/17/17.
+//  Copyright © 2017 Mock. All rights reserved.
+//
+
+import UIKit
+
+class DeviceControlTableViewCell: UITableViewCell {
+
+
+
+}
