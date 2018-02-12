e2e0ebd51005552df7acdecc7ad21e06f1c451c2
Will <will@mockdesign.com>
2018-02-12 16:25:00 -0600

    Added checks for refreshing the tokens and keeping users logged in

--- ReactorHome/ReactorHome/AppDelegate.swift
+++ ReactorHome/ReactorHome/AppDelegate.swift
@@ -14,9 +14,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch
+        
+        let validOauth = oauthValidationCheck()
+        
+        if (validOauth){
+            
+            print("finished login check")
+            let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+            let tabBarController: UITabBarController = storyboard.instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
+            self.window?.makeKeyAndVisible()
+            self.window?.rootViewController = tabBarController
+            
+        }else{
+            print("user is not logged in. Send to the login screen for auth")
+            let preferences = UserDefaults.standard
+            
+            guard let expires_at = preferences.object(forKey: "expires_at") as? Date else{
+                print("No Expiration set in user defaults. Something went wrong in refreshAuth / oauthVaildationCheck()")
+                return true
+            }
+            
+            print(expires_at)
+            //print(UserDefaults.standard.object(forKey: "expires_at")!)
+        }
+        
         return true
     }
 
