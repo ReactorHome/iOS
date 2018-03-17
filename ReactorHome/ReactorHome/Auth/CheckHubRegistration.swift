5313b7043dd66a90e27f93ae63fc33eaba5e2f4d
Will Mock <WillMock1@wills-mbp.lan>
2018-03-17 14:05:58 -0500

    small changes for auth

---
+++ ReactorHome/ReactorHome/Auth/CheckHubRegistration.swift
@@ -0,0 +1,14 @@
+//
+//  CheckHubRegistration.swift
+//  ReactorHome
+//
+//  Created by Will Mock on 2/28/18.
+//  Copyright © 2018 Mock. All rights reserved.
+//
+
+import Foundation
+
+//this function will be used in order to check if the user has registered a hub to their account. If they have it will proced to the dashboard, if not it change them to a different VC that then will ask them to scan the hub QR code.
+func checkHubRegistration() -> Bool{
+    return true
+}
