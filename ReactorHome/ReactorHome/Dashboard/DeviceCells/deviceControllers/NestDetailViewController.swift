bbb4d2d09ae9b8b1dfb1c0f75a1dd4c1016edcb4
Will Mock <WillMock1@wills-mbp.lan>
2018-04-04 22:24:45 -0500

    added more segues and VCs in order to have detail pages for other devices

---
+++ ReactorHome/ReactorHome/Dashboard/DeviceCells/deviceControllers/NestDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NestDetailViewController.swift
+//  ReactorHome
+//
+//  Created by Will Mock on 4/4/18.
+//  Copyright © 2018 Mock. All rights reserved.
+//
+
+import UIKit
+
+class NestDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
